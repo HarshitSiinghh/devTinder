db6ff370c6f8dcfa11a91e03357257b094d2c884
HarshitSiinghh <harshitsingh0824@gmail.com>
2025-10-14 21:38:56 +0530

    understanding all get post use send methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 let app = express();
-app.use("/harshit", (req, res) => {
-  res.send(" harshit ");
+
+//this will only handle get aall methods / user
+app.get("/user",(req, res) => {
+   res.send({firstName:"Harshit ", lastName:"singh"})
+
+})
+app.post("/user",(req, res) => {
+ //saving data to dB
+
+   res.send("data saved to the database")
+
+})
+
+app.delete("/user",(req, res) => {
+ //saving data to dB
+
+   res.send("deleted successfully")
+
+})
+
+
+//this will send them all http method API call to /test 
+app.use("/test", (req, res) => {
+  res.send("hello form the test");
 });
-app.use("/singh", (req, res) => {
-  res.send("hello form the servrr singh");
-});
+
+
 
 app.listen(7777, () => {
   console.log("server is successfully listing to on port 7777");
