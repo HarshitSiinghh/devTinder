1bf12120f3a1cff81f9e8182cf91d4fb28b7cf9c
HarshitSiinghh <harshitsingh0824@gmail.com>
2025-10-22 21:43:08 +0530

    playing with APIS

--- src/hello.js
+++ src/hello.js
@@ -1,23 +1,19 @@
 
-var nextGreaterElement = function(nums1, nums2) {
-    let stack =[]
-    for(let i =0; i<nums1.length;i++){
-      let next =-1
-        let index = nums2.indexOf(nums1[i]);
-        for(let j= index ; j<nums2.length; j++){
-            if(nums1[i]<nums2[j]){
-                next = nums2[j]
-                break
-            }
-            else{
-                
-             
-            }
-        }
-        stack.push(next)
+var sumDivisibleByK = function(nums, k) {
+    let result = 0;
+    let count =1
+    for(let i=0; i<nums.length; i++){
+         if( nums[i] === nums[i+1]){
+             ++count 
+         }
+         if( nums[i] !== nums[i+1]){
+             if(count % k === 0){
+                  result= result + nums[i]* count 
+             count =1
+                }
+              
+         }
     }
-     return stack
-    
+     return  result
 };
-
-console.log(nextGreaterElement([4,1,2], [1,3,4,2]));
+console.log(sumDivisibleByK([1,2,2,3,3,3,3,4],  2));
--- src/index.js
+++ src/index.js
@@ -1,35 +1,99 @@
- const User = require("./Models/user")
+const User = require("./Models/user");
 const express = require("express");
- const connectDB = require("./config/database")
- 
+const connectDB = require("./config/database");
+const e = require("express");
+
 let app = express();
 
+app.use(express.json());
+
+app.patch("/user", async (req,res)=>{
+  const getEle = req.body._id
+  try{
+    const updateEle = await User.findByIdAndUpdate(getEle,{ firstName:"lordHarshit" })
+    if( updateEle.length ===0){
+      res.status(400).send(' user update Unsuccessfull')
+    }
+    else{
+      res.send(updateEle)
+    }
+
+  }catch (err){
+      res.send("error in this  operation in th catch block")
 
 
-app.post("/user", async  (req , res)=>{
-  const  user = new User({
-    firstName :"virat ",
-    lastName: "kohli",
-    age: 20,
-    email: "viratkohli@gmail.com",
-    gender: "male"
-  }) 
-
-
-  await user.save()
- res.send("user addes successfully")
+     }
 })
 
+app.delete("/user", async(req,res)=>{
+  const userID = req.body._id
+  try{
+    const deteletID = await User.deleteOne({_id: userID})
+     if( deteletID.length ===0){
+      res.status.send("not find any person with is ID")
 
-connectDB().then(()=>{
-  console.log("connect to th DB is successfully");
-  app.listen(7777, () => {
-  console.log("server is successfully listing to on port 7777");
+     }
+     else{
+      res.send("user deleted successefully ")
+     }
+    
+
+     }catch (err){
+      res.send("error in this  operation in th catch block")
+
+
+     }
+  
+})
+
+app.get("/getuserbyname", async (req, res) => {
+  const userId = req.body._id;
+  try {
+    const userById = await User.findById({ _id: userId });
+    if (userById.length === 0) {
+      res.status(400).send("Bad request");
+    } else {
+      res.send(userById);
+    }
+  } catch (err) {
+    res.status(404).send("user is not founded ");
+  }
 });
-  
-}).catch((err)=>{
-  console.log("cannot connect to DB the is a error"); 
-  
-})
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const findByMailUser = await User.find({ email: userEmail });
+    if (findByMailUser.length === 0) {
+      res.status(404).send("user is not found");
+    } else {
+      res.send(findByMailUser);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.send("user addes successfully");
+  } catch (err) {
+    res.status(400).send("err saving the user :" + err.message);
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("connect to th DB is successfully");
+    app.listen(7777, () => {
+      console.log("server is successfully listing to on port 7777");
+    });
+  })
+  .catch((err) => {
+    console.log("cannot connect to DB the is a error");
+  });
